68556b5fd59130559714fc5189aefa16fbb36ac2
PIYUSH SINGH <piyushsingh9862@gmail.com>
2023-01-19 17:39:25 +0530

    Initial Commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Piyush Singh on 06/01/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Piyush Singh on 06/01/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBOutlet weak var rightImageview: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
